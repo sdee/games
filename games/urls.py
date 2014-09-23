fab0efa4e3ddca67e0c72247a762de6f2b7745d8
sdee <sdee@Sutees-MacBook-Air.local>
2014-09-22 20:52:59 -0700

    Cleans up route

--- games/urls.py
+++ games/urls.py
@@ -3,13 +3,10 @@ from django.contrib import admin
 # from inventory.models import games
 from inventory import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'games.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index, name='index'),
-    url(r'^game/(?P<game_id>\d+)/$$', views.detail, name='detail'),
-    url(r'^game/new/$$', views.new, name='new'),
+urlpatterns = patterns(
+  '',
+  url(r'^$', views.index, name='index'),
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^game/(?P<game_id>\d+)/$$', views.detail, name='detail'),
+  url(r'^game/new/$$', views.new, name='new'),
 )
