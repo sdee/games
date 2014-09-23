f996023eb35bd0becb7b479c1e5f1f2845a1bb5e
sdee <sdee@Sutees-MacBook-Air.local>
2014-09-23 02:27:19 -0700

    attempt to integrate bootgrid and xeditable

--- inventory/static/js/init.js
+++ inventory/static/js/init.js
@@ -1,1 +1,13 @@
 $("#grid-basic").bootgrid();
+$.fn.editable.defaults.mode = 'inline';
+$(document).ready(function() {
+
+    $(".editloc").editable(
+      {
+    type: 'text',
+    pk: 1,
+    url: '/post',
+    title: 'Enter username'
+}
+    );
+});
