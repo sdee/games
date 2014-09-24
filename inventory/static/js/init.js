ff9210e0e618b93d7e8ba3e5c9c5358706ccc5a5
sdee <sdee@Sutees-MacBook-Air.local>
2014-09-23 22:22:34 -0700

    Purges old datatable js

--- inventory/static/js/init.js
+++ inventory/static/js/init.js
@@ -1,13 +1,4 @@
-$("#grid-basic").bootgrid();
-$.fn.editable.defaults.mode = 'inline';
 $(document).ready(function() {
 
-    $(".editloc").editable(
-      {
-    type: 'text',
-    pk: 1,
-    url: '/post',
-    title: 'Enter username'
-}
-    );
+
 });
